d1652cd539442b89ed28d0cb21c3b405b86229a7
Jack Maguire <jackmaguire1444@gmail.com>
2022-01-26 17:33:05 -0600

    cleaner build script

--- build.sh
+++ build.sh
@@ -6,37 +6,23 @@
 #ln -s ../wt/resources/
 
 WARN="-Wall -Wshadow -Wunused -pedantic -Wextra -Werror"
-#WARN="$WARN -Wno-subobject-linkage"
+WARN="$WARN -Wno-subobject-linkage"
 
-#DEBUG="-D_GLIBCXX_DEBUG"
+DEBUG="-D_GLIBCXX_DEBUG"
 #DEBUG="$DEBUG -g"
 
-#include="$include -I /root/wt_util/include"
+opt="-O3"
+
 include="$include -I ."
 include="$include -I extern/json/single_include/"
-#include="$include -isystem tf/include"
-#include="$include -isystem cppflow/include"
-#include="$include -isystem wt_build_src"
 include="$include -isystem extern/wt/src"
 
 libraries="$libraries -lwt"
 libraries="$libraries -lwthttp"
-#libraries="$libraries -ltensorflow"
-#libraries="$libraries -lboost_signals"
-#libraries="$libraries -lstdc++fs"
-#libraries="$libraries -L."
-#libraries="$libraries -Ltf/lib"
 libraries="$libraries -Lextern/wt/build/src"
 libraries="$libraries -Lextern/wt/build/src/http"
 libraries="$libraries -L/usr/lib/x86_64-linux-gnu/"
 
-opt="-O3"
-
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/tf/lib/
-
-#clang++ --stdlib=libc++
-
 cmd="g++-11 --std=c++2a -o run_unit_tests run_unit_tests.cc $opt $libraries $include -Wl,-rpath,. $WARN $DEBUG"
 echo $cmd
 $cmd
-#g++-11 --std=c++2a -o run_unit_tests run_unit_tests.cc $opt $libraries $include -Wl,-rpath,. $WARN $DEBUG
