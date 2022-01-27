b0264b0235c312bf69b5eea6857e602e0f39c891
Jack Maguire <jackmaguire1444@gmail.com>
2022-01-26 19:45:25 -0600

    build_app.sh apps/main.cc

---
+++ apps/main.cc
@@ -0,0 +1,55 @@
+#include <Wt/WApplication.h>
+#include <Wt/WBootstrapTheme.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WBorderLayout.h>
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "gtt/rep/note.hh"
+#include "gtt/rep/gtrstring.hh"
+#include "gtt/rep/guitar.hh"
+#include "gtt/rep/measure.hh"
+#include "gtt/rep/track.hh"
+
+
+#include "example_songs/spirited_away_intro.hh"
+
+using namespace Wt;
+using namespace gtt;
+using namespace gtt::rep;
+
+//using MyBoardWidget = BoardWidget< RobotsGame< NullVisualizer, true, 100 > >;
+//using MyBoardWidget = BoardWidget< RobotsGame< NullVisualizer, true, 0 > >;
+
+std::unique_ptr< Wt::WApplication >
+createApplication( Wt::WEnvironment const & env ) {
+
+  std::unique_ptr< Wt::WApplication > app = Wt::cpp14::make_unique< Wt::WApplication >( env );
+  app->setTitle( "Guitar Tab Transcriber" );
+  app->root()->setStyleClass( "root" );
+  
+  Wt::WBorderLayout * const border =
+    app->root()->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+  //auto score = Wt::cpp14::make_unique< ScoreWidget >( );
+  //ScoreWidget * raw_score = &(*score);
+  //border->addWidget( std::move( score ),  Wt::LayoutPosition::East );
+
+  //border->addWidget( Wt::cpp14::make_unique< MyBoardWidget >( raw_score, &(*app) ),  Wt::LayoutPosition::Center );
+ 
+  
+  //app->root()->addWidget( Wt::cpp14::make_unique< MyBoardWidget >( ) );
+
+  app->setCssTheme( "polished" );
+  app->setTheme( std::make_shared< Wt::WBootstrapTheme >() ) ;
+
+  return app;
+}
+
+int main( int argc, char **argv )
+{
+  return WRun( argc, argv, &createApplication );
+}
---
+++ build_app.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+app_name=$1
+
+#find ../wt/build/src/ | grep "\.so" | xargs -n1 ln -s
+#ln -s ../wt/build/src/http/libwthttp.so libwthttp2.so
+#ln -s ../wt/build/src/libwt.so libwt2
+#ln -s ../wt/resources/
+
+WARN="-Wall -Wshadow -Wunused -pedantic -Wextra -Werror"
+WARN="$WARN -Wno-subobject-linkage"
+
+DEBUG="-D_GLIBCXX_DEBUG"
+#DEBUG="$DEBUG -g"
+
+opt="-O3"
+
+include="$include -I ."
+include="$include -I extern/json/single_include/"
+include="$include -isystem extern/wt/src/"
+include="$include -isystem extern/wt/build/"
+
+libraries="$libraries -lwt"
+libraries="$libraries -lwthttp"
+libraries="$libraries -Lextern/wt/build/src"
+libraries="$libraries -Lextern/wt/build/src/http"
+libraries="$libraries -L/usr/lib/x86_64-linux-gnu/"
+
+mkdir bin 2>/dev/null
+cmd="g++-11 --std=c++2a -o bin/$app_name apps/${app_name}.cc $opt $libraries $include -Wl,-rpath,. $WARN $DEBUG"
+echo $cmd
+$cmd
