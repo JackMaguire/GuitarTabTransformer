205daeadd8a3546d3145ecfd6519a4c9dbce6d0c
Jack Maguire <jackmaguire1444@gmail.com>
2022-02-12 20:46:21 -0600

    run_Measure_unit_tests_5 is giving trouble for some reason

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -20,8 +20,8 @@ def run_Note_unit_tests():
     g3 = n - X.oct
     assert( g3.letter() == NoteLetter.G )
     assert( g3.octave() == 3 )
-    
-    
+
+
     # Block 2
     n = Note( "Ab/0" )
     assert( n.letter() == NoteLetter.Ab )
@@ -42,7 +42,7 @@ def run_Note_unit_tests():
     assert( Note( "G/5" ) != Note( "A/4" ) );
 
     print( "Passed Note Tests" )
-    
+
 def run_GtrString_unit_tests():
     s = GtrString( "Ab/5", 24 )
     assert( s.get_fret( s.open_string_note() ) == 0 )
@@ -92,18 +92,121 @@ def run_Guitar_unit_tests():
     g[3] = g[0]
     assert( g[3].open_string_note().as_int() ==
             Note("G/2").as_int() );
-    
+
     print( "Passed Guitar Tests" )
 
+
+def run_Measure_unit_tests_5():
+    # Block 5: Overlapping notes
+    m = Measure({
+	MeasureNote( "Ab/5", 0.00, 0.25 ),
+	MeasureNote( "E/4",  0.25, 0.25 ),
+	MeasureNote( "E/7",  0.25, 0.125 ),
+	MeasureNote( "Gb/6", 0.75, 0.25 )
+    })
+
+    rests5 = m.compute_rests()
+    assert( len(rests5) == 1 )
+
+    rest5 = list(rests5)[0]
+    print( rest5.starting_point )
+    assert( rest5.starting_point == 0.5 );
+    assert( rest5.length == 0.25 );
+    assert( rest5.ending_point() == 0.75 );
+
+
+    # Block 5: Auto-assign strings
+    g = GuitarFactory.standard_guitar()
+    m = Measure({
+	MeasureNote( Note("G/3")+3, g, 0.00, 0.25 ),
+        MeasureNote( Note("A/2"),   g, 0.25, 0.25 ),
+	MeasureNote( Note("D/3")-1, g, 0.75, 0.25 )
+    })
+    assert( m[0].string_assignment == 2 )
+    assert( m[1].string_assignment == 4 )
+    assert( m[2].string_assignment == 4 )
 
 def run_Measure_unit_tests():
     # Block 1
     m = Measure({
 	MeasureNote( "Ab/5", 0.00, 0.25 ),
 	MeasureNote( "E/4",  0.25, 0.50 ),
-	MeasureNote( "Gb/6", 0.75, 0.25 )        
+	MeasureNote( "Gb/6", 0.75, 0.25 )
     })
-    assert( m[0].starting_point == 0 )
+
+    assert( m.size() == 3 );
+    assert( m[0].starting_point == 0.0 );
+    assert( m[1].starting_point == 0.25 );
+    assert( m[2].starting_point == 0.75 );
+    assert( m[0].length == 0.25 );
+    assert( m[1].length == 0.50 );
+    assert( m[2].length == 0.25 );
+    assert( m[0].note == Note("Ab/5") );
+    assert( m[1].note == Note( "E/4") );
+    assert( m[2].note == Note("Gb/6") );
+
+    rests = m.compute_rests()
+    assert( len(rests) == 0 )
+
+
+    # Block 2: Out of order
+    m = Measure({
+	MeasureNote( "E/4",  0.25, 0.50 ),
+	MeasureNote( "Ab/5", 0.00, 0.25 ),
+	MeasureNote( "Gb/6", 0.75, 0.25 )
+    })
+
+    assert( m.size() == 3 );
+    assert( m[0].starting_point == 0.0 );
+    assert( m[1].starting_point == 0.25 );
+    assert( m[2].starting_point == 0.75 );
+
+
+    # Block 3: Single rest
+    m = Measure({
+	MeasureNote( "Ab/5", 0.00, 0.25 ),
+	MeasureNote( "E/4",  0.25, 0.25 ),
+	MeasureNote( "Gb/6", 0.75, 0.25 )
+    })
+    assert( m.size() == 3 );
+
+    rests = m.compute_rests()
+    assert( len(rests) == 1 )
+
+    rest = list(rests)[0]
+    assert( rest.starting_point == 0.5 );
+    assert( rest.length == 0.25 );
+    assert( rest.ending_point() == 0.75 );
+
+
+    # Block 4: Multiple rest
+    m = Measure({
+	MeasureNote( "Ab/5", 0.25, 0.125 ),
+	MeasureNote( "Gb/6", 0.75, 0.125 )
+    })
+    assert( m.size() == 2 );
+
+    rests = sorted( m.compute_rests() )
+    assert( len(rests) == 3 )
+
+    rest = list(rests)[0]
+    assert( rest.starting_point == 0.0 );
+    assert( rest.length == 0.25 );
+    assert( rest.ending_point() == 0.25 );
+
+    rest = list(rests)[1]
+    assert( rest.starting_point == 0.375 );
+    assert( rest.length == 0.375 );
+    assert( rest.ending_point() == 0.75 );
+
+    rest = list(rests)[2]
+    assert( rest.starting_point == 0.875 );
+    assert( rest.length == 0.125 );
+    assert( rest.ending_point() == 1.0 );
+
+
+
+    run_Measure_unit_tests_5()
 
     print( "Passed Measure Tests" )
 
