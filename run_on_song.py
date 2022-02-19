929b4251716bde34839cb5f4b7f9846a7b261fe2
Jack Maguire <jackmaguire1444@gmail.com>
2022-02-19 13:08:37 -0600

    bounds checking

--- run_on_song.py
+++ run_on_song.py
@@ -9,12 +9,25 @@ import time
 
 class Settings:
     def __init__(self):
-        self.measure_buffer = 2
+        self.measure_buffer = 0
         self.active_measure_width = 32
         self.total_measure_width = self.active_measure_width+(2*self.measure_buffer)
 
         self.m_per_row = 3
         self.row_gap = 3
+
+        self.x_gap = 5
+
+        self.beat_size = self.active_measure_width / 4
+
+    def x_is_beat( self, x ):
+        if x < self.measure_buffer: return False
+
+        x = x - self.measure_buffer
+
+        if x >= self.active_measure_width: return False
+        
+        return x % self.beat_size == 0
 
 class Cursor:
     def __init__(self):
@@ -32,9 +45,11 @@ def measure_ind_to_xy( index, settings: Settings, g: gtt.Guitar ):
     y = 3 + j_offset*(settings.row_gap+g.size())
 
     i_offset = index % settings.m_per_row
-    x = 3 + i_offset*(settings.total_measure_width+1)
+    x = settings.x_gap + i_offset*(settings.total_measure_width+1)
 
-    return x, y
+    newline = i_offset==0
+
+    return x, y, newline
 
     
 
@@ -46,16 +61,29 @@ def draw_measure(
         m_ind: int,
         settings: Settings
 ):
+    noteless_color = curses.color_pair(242)
+
     #################
     # draw background
-
-    start_x, start_y = measure_ind_to_xy( m_ind, settings, g )
+    start_x, start_y, is_newline = measure_ind_to_xy( m_ind, settings, g )
     #print( m_ind, start_x, start_y )
     end_x = start_x + settings.total_measure_width
     end_y = start_y + g.size()
     for x in range( start_x, end_x ):
+        if settings.x_is_beat( x - start_x ):
+            color = curses.color_pair(252)
+        else:
+            color = noteless_color
+
         for y in range( start_y, end_y ):
-            stdscr.addch( y, x, '-', curses.color_pair(246) )
+            stdscr.addch( y, x, '-', color )
+
+    if is_newline:
+        for i in range( 0, g.size() ):
+            y = start_y + i
+            x = 1
+            s = g.get_string(i).open_string_note().as_string()
+            stdscr.addstr( y, x, s, noteless_color )
 
     ############
     # draw notes
@@ -69,12 +97,12 @@ def draw_measure(
 
     ############
     # draw rests
-    rests = m.compute_rests()
-    for r in rests:
-        #y = start_y + note.string_assignment
-        x = start_x + settings.measure_buffer + int( settings.active_measure_width * r.starting_point )
-        #fret = g.get_string( note.string_assignment ).get_fret( note.note )
-        #stdscr.addstr( y, x, str(fret) )
+    if False:
+        rests = m.compute_rests()
+        for r in rests:
+            x = start_x + settings.measure_buffer + int( settings.active_measure_width * r.starting_point )
+            for y in range( start_y, end_y ):
+                stdscr.addch( y, x, '~' )
 
 def draw_track( stdscr, track, cursor: Cursor, settings: Settings ):
     count = 0
@@ -89,51 +117,81 @@ def main( stdscr ):
     stdscr.clear()
     stdscr.border(0)
 
-    user_cursor = Cursor()
-
     writer_cursor = Cursor()
     settings = Settings()
 
     track = Track( "example_songs/spirited_away_intro.json" )
 
-    draw_track( stdscr, track, writer_cursor, settings )
+    #draw_track( stdscr, track, writer_cursor, settings )
     #stdscr.refresh()
-    time.sleep( 10 )
+    #time.sleep( 10 )
+
+    def moveto( y, x ):
+        maxy, maxx = stdscr.getmaxyx()
+
+        if y > maxy-1: y = maxy-1
+        elif y < 1: y = 1
+
+        if x > maxx-1: x = maxx-1
+        elif x < 1: x = 1
+
+        stdscr.move( y, x )
+
 
     while True:
+        c_pos = stdscr.getyx()
+        draw_track( stdscr, track, writer_cursor, settings )
+        moveto( *c_pos )
+
         stdscr.refresh()
         k = stdscr.get_wch()
 
         if k in ( 'Q', 'q' ):
-            user_cursor.x -= 1
-            user_cursor.y += 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x -= 1
+            y += 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'W', 'w' ):
-            user_cursor.y += 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            y += 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'E', 'e' ):
-            user_cursor.x += 1
-            user_cursor.y += 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x += 1
+            y += 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'A', 'a' ):
-            user_cursor.x -= 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x -= 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'S', 's' ):
             pass
         elif k in ( 'D', 'd' ):
-            user_cursor.x += 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x += 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'Z', 'z' ):
-            user_cursor.x -= 1
-            user_cursor.y -= 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x -= 1
+            y -= 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'X', 'x' ):
-            user_cursor.y -= 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            y -= 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
         elif k in ( 'C', 'c' ):
-            user_cursor.x += 1
-            user_cursor.y -= 1
-            draw_game( stdscr, game, show_pocket )
+            y, x = stdscr.getyx()
+            x += 1
+            y -= 1
+            moveto( y, x )
+            #draw_game( stdscr, game, show_pocket )
     
 
 if __name__ == '__main__':
