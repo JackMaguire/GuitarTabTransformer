a918f2ebec556c55e2dba3b25a1c862ae7e2ece8
Jack Maguire <jackmaguire1444@gmail.com>
2022-02-19 11:38:57 -0600

    can view measures

--- run_on_song.py
+++ run_on_song.py
@@ -1,8 +1,25 @@
+import gtt
 from gtt import *
 import gtt.shortcuts as X
 
 import curses
 from curses import wrapper
+
+import time
+
+class Settings:
+    def __init__(self):
+        self.measure_buffer = 2
+        self.active_measure_width = 32
+        self.total_measure_width = self.active_measure_width+(2*self.measure_buffer)
+
+        self.m_per_row = 3
+        self.row_gap = 3
+
+class Cursor:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
 
 def close( stdscr ):
     curses.nocbreak()
@@ -10,55 +27,96 @@ def close( stdscr ):
     curses.echo()
     curses.endwin()
 
+def measure_ind_to_xy( index, settings: Settings, g: gtt.Guitar ):
+    j_offset = int( index / settings.m_per_row )
+    y = 3 + j_offset*(settings.row_gap+g.size())
+
+    i_offset = index % settings.m_per_row
+    x = 3 + i_offset*(settings.total_measure_width+1)
+
+    return x, y
+
+    
+
+def draw_measure(
+        stdscr,
+        m: gtt.Measure,
+        g: gtt.Guitar,
+        #cursor: Cursor,
+        m_ind: int,
+        settings: Settings
+):
+    start_x, start_y = measure_ind_to_xy( m_ind, settings, g )
+    print( m_ind, start_x, start_y )
+    end_x = start_x + settings.total_measure_width
+    end_y = start_y + g.size()
+    for x in range( start_x, end_x ):
+        for y in range( start_y, end_y ):
+            stdscr.addch( y, x, '-', curses.color_pair(84) )
+
+def draw_track( stdscr, track, cursor: Cursor, settings: Settings ):
+    count = 0
+    g = track.guitar
+    for m in track.measures:
+        #print( count, m )
+        draw_measure( stdscr, m, g, count, settings )
+        count += 1
+
 def main( stdscr ):
     # Clear screen
     stdscr.clear()
     stdscr.border(0)
 
-    cursor_X = 0
-    cursor_Y = 0
-    measures_per_row = 1
-    row_offset = 0
+    user_cursor = Cursor()
+
+    writer_cursor = Cursor()
+    settings = Settings()
+
+    track = Track( "example_songs/spirited_away_intro.json" )
+
+    draw_track( stdscr, track, writer_cursor, settings )
+    #stdscr.refresh()
+    #time.sleep( 10 )
 
     while True:
         stdscr.refresh()
         k = stdscr.get_wch()
 
         if k in ( 'Q', 'q' ):
-            cursor_X -= 1
-            cursor_Y += 1
+            user_cursor.x -= 1
+            user_cursor.y += 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'W', 'w' ):
-            cursor_Y += 1
+            user_cursor.y += 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'E', 'e' ):
-            cursor_X += 1
-            cursor_Y += 1
+            user_cursor.x += 1
+            user_cursor.y += 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'A', 'a' ):
-            cursor_X -= 1
+            user_cursor.x -= 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'S', 's' ):
             pass
         elif k in ( 'D', 'd' ):
-            cursor_X += 1
+            user_cursor.x += 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'Z', 'z' ):
-            cursor_X -= 1
-            cursor_Y -= 1
+            user_cursor.x -= 1
+            user_cursor.y -= 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'X', 'x' ):
-            cursor_Y -= 1
+            user_cursor.y -= 1
             draw_game( stdscr, game, show_pocket )
         elif k in ( 'C', 'c' ):
-            cursor_X += 1
-            cursor_Y -= 1
+            user_cursor.x += 1
+            user_cursor.y -= 1
             draw_game( stdscr, game, show_pocket )
     
 
 if __name__ == '__main__':
-    t = Track( "example_songs/spirited_away_intro.json" )
-    print( t )
+    #t = Track( "example_songs/spirited_away_intro.json" )
+    #print( t )
 
     stdscr = curses.initscr()
     curses.noecho()
@@ -70,6 +128,6 @@ if __name__ == '__main__':
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, curses.COLOR_BLACK, i)
 
-    #wrapper(main)
+    wrapper(main)
 
     close( stdscr )
