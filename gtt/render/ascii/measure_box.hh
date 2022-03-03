ea3ed796ff52ef48ce45338c84e84700e97cbc0f
Jack Maguire <jackmaguire1444@gmail.com>
2022-03-02 20:50:13 -0600

    initialize

--- gtt/render/ascii/measure_box.hh
+++ gtt/render/ascii/measure_box.hh
@@ -4,6 +4,8 @@
 #include "gtt/rep/measure.hh"
 #include "gtt/rep/guitar.hh"
 
+#include <string>
+#include <vector>
 
 namespace gtt {
 namespace render {
@@ -12,6 +14,26 @@ namespace ascii {
 struct MeasureBoxSettings {
   int measure_width = 24;
   rep::TimeSignature time_sig;
+
+  bool x_is_beat( int const x ) const {
+    //if( x < 0 ) return false;
+    //if( x >= measure_width ) return false;
+    int const beat_size = measure_width / time_sig.beats_per_measure();
+    return x % beat_size == 0;
+  }
+
+  std::vector< int >
+  beat_positions() const {
+    std::vector< int > beats;
+    beats.reserve( time_sig.beats_per_measure() );
+
+    int const beat_size = measure_width / time_sig.beats_per_measure();
+    for( int i = 0; i < time_sig.beats_per_measure(); ++i ){
+      beats.push_back( i * beat_size );
+    }
+
+    return beats;
+  }
 };
 
 class MeasureBox {
@@ -60,6 +82,33 @@ MeasureBox::initialize(
   constexpr CharVal empty_note({ '-', 242 });
   constexpr std::vector empty_string( settings_.measure_width, empty_note );
   render_.resize( g.size(), empty_string );
+
+  // color the beats
+  for( int const x : settings_->beat_positions() ){
+    for( int y = 0; y < g.size(); ++y ){
+      render_[y][x].color = 252;
+    }
+  }
+
+  // draw notes
+  for( MeasureNote const & mnote : *measure ){
+    int const y = mnote.string_assignment;
+    int const x = int( settings_->measure_width * mnote.starting_point );
+    int const fret = g.get_string( y ).get_fret( mnote.note );
+    std::string const fret_str = std::to_string( fret );
+    if( fret_str.size() == 1 ){
+      render_[y][x].c = fret_str[ 0 ];
+    } else {
+      GTT_ASSERT_EQUALS( fret_str.size(), 2 );
+      if( x == 0 ){
+	render_[y][x].c = fret_str[ 0 ];
+	render_[y][x+1].c = fret_str[ 1 ];
+      } else {
+	render_[y][x-1].c = fret_str[ 0 ];
+	render_[y][x].c = fret_str[ 1 ];
+      }
+    }
+  }
 }
 
 
