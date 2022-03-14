7ce011cd9611c064b630ec04c3c65ccc18745276
Jack Maguire <jackmaguire1444@gmail.com>
2022-03-13 21:01:31 -0500

    cleaned up state_cache.hh

--- gtt/render/ascii/state_cache.hh
+++ gtt/render/ascii/state_cache.hh
@@ -15,45 +15,16 @@ public:
   }
 
   void
-  register_new_state( std::string const & state ){
-    //Can't be empty
-
-    // skip non-updates
-    if( *current_iter_ == state ) return;
-
-    // delete future if there is any
-    all_states_.erase( all_states_.begin(), current_iter_ );
-
-    // save this state
-    all_states_.push_front( state );
-    current_iter_ = all_states_.begin();
-  }
+  register_new_state( std::string const & state );
 
   std::string
-  get_current_state() const {
-    if( current_iter_ != all_states_.end() ) {
-      return *current_iter_;
-    } else {
-      return "";
-    }
-  }
+  get_current_state() const;
 
   std::string
-  undo(){
-    //if( all_states_.empty() ) return "";
-    if( current_iter_ != all_states_.end() and std::next(current_iter_) != all_states_.end() ){
-      ++current_iter_;
-    }
-    return *current_iter_;
-  }
+  undo();
 
   std::string
-  redo(){
-    if( current_iter_ != all_states_.begin() ){
-      --current_iter_;
-    }
-    return *current_iter_;
-  }
+  redo();
 
   auto size() const {
     return all_states_.size();
@@ -65,6 +36,46 @@ private:
   std::list< std::string > all_states_;
   decltype(all_states_)::iterator current_iter_ = all_states_.begin();
 };
+
+void
+StateCache::register_new_state( std::string const & state ){
+  //Can't be empty
+
+  // skip non-updates
+  if( *current_iter_ == state ) return;
+
+  // delete future if there is any
+  all_states_.erase( all_states_.begin(), current_iter_ );
+
+  // save this state
+  all_states_.push_front( state );
+  current_iter_ = all_states_.begin();
+}
+
+std::string
+StateCache::get_current_state() const {
+  if( current_iter_ != all_states_.end() ) {
+    return *current_iter_;
+  } else {
+    return "";
+  }
+}
+
+std::string
+StateCache::undo(){
+  if( current_iter_ != all_states_.end() and std::next(current_iter_) != all_states_.end() ){
+    ++current_iter_;
+  }
+  return *current_iter_;
+}
+
+std::string
+StateCache::redo(){
+  if( current_iter_ != all_states_.begin() ){
+    --current_iter_;
+  }
+  return *current_iter_;
+}
 
 } // ascii
 } // render
